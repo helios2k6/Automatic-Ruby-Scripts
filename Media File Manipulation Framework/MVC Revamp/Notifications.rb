47bd64e9fdbc463edf119a5617ec08892cd40611
helios2k6 <helios2k6@gmail.com>
2012-02-27 17:38:19 -0500

    Added some extra notifications

--- Media File Manipulation Framework/MVC Revamp/Notifications.rb
+++ Media File Manipulation Framework/MVC Revamp/Notifications.rb
@@ -1,10 +1,11 @@
 require 'PureMVC_Ruby'
 module Notifications
 	class Notifications
-		UPDATE_SCREEN = "UPDATE_SCREEN"
-		EXECUTE_COMMAND = "EXECUTE_COMMAND"
+		UPDATE_SCREEN = "UPDATE_SCREEN" #Used to instruct the screen to refresh with some sort of message
+		EXECUTE_COMMAND = "EXECUTE_COMMAND" #Used to instruct the execution of a command
 		
-		COMMAND_EXECUTED = "COMMAND_EXECUTED"
+		COMMAND_EXECUTED = "COMMAND_EXECUTED" #Used to signal that a command has been executed
+    COMMAND_FINISHED_EXECUTING = "COMMAND_FINISHED_EXECUTING" #Used to signal that a command has finished executing
     
 	end
 end
