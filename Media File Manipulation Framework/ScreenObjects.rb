fb20d74f71c67981557bab84a7d5330c3e0795cc
helios2k6 <helios2k6@gmail.com>
2012-05-28 22:30:21 -0400

    updated requirements to have rubygems

--- Media File Manipulation Framework/ScreenObjects.rb
+++ Media File Manipulation Framework/ScreenObjects.rb
@@ -12,6 +12,7 @@
 #
 #You should have received a copy of the GNU General Public License
 #along with Auto Device Encoder.  If not, see <http://www.gnu.org/licenses/>.
+require 'rubygems'
 require 'PureMVC_Ruby'
 require './Constants'
 module ScreenObjects
