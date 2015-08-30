68ad914e286dcc6e2fa40f2a71215c97e3e84514
Andrew Johnson <helios2k6@gmail.com>
2015-08-30 09:30:12 -0400

    Adding updates

--- FileRenamingUtilities.rb
+++ FileRenamingUtilities.rb
@@ -7,19 +7,28 @@ module FileRenamingUtilities
         end
         
         def self.renameFilesInFolder(rootNameString, files, startIndex=1)
+            pictureFiles = []
             tempFiles = []
             i = startIndex
+            # Grab all the picture files. We have to prepopulate this 
+            # because if we don't, then when we go to rename a file, the
+            # "files iterator" will actually yield the temporary file and
+            # we'll end up renaming the temporary file over and over again 
+            # until we hit the INT_MAX limit
             files.each{|f|
                 ext = File.extname(f)
                 if isFilePicture?(f) then
-                    ext = File.extname(f)
-                    properName = File.basename(f, ext)
-                    tempName = "TEMP_NAME (#{i})#{ext}"
-                    File.rename(f, tempName)
-                    puts("rename #{f} to #{tempName}")
-                    tempFiles << tempName
-                    i = i + 1
+                    pictureFiles << f
                 end
+            }
+            pictureFiles.each{|f|
+                ext = File.extname(f)
+                properName = File.basename(f, ext)
+                tempName = "TEMP_NAME (#{i})#{ext}"
+                File.rename(f, tempName)
+                puts("rename #{f} to #{tempName}")
+                tempFiles << tempName
+                i = i + 1
             }
             
             i = startIndex
